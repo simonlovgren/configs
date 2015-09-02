07d851d1d7bb80190e955380c40576efb9db782e
Simon Lövgren <lovgren.simon@gmail.com>
2015-09-02 16:32:43 +0200

    updated init.el

--- init.el
+++ init.el
@@ -1,3 +1,11 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
+
+
+;; Turn off splash
+(setq inhibit-splash-screen t)
+;; Turn on line number and column mode
+(column-number-mode t)
+;; Turn on parenthesis mode
+(show-paren-mode t)
