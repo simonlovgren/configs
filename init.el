5d201c83f36e979eef90cc7a2e3f44da6c19c251
Simon Lövgren <lovgren.simon@gmail.com>
2015-10-13 14:09:25 +0200

    added elpa

--- init.el
+++ init.el
@@ -27,6 +27,9 @@
              '("melpa" . "https://melpa.org/packages/"))
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives
+             '("org" . "HTTP://orgmode.org/elpa/"))
+
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
